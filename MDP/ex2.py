0a3c3523711aa52255f9924ed87832fa5316e869
Nadav Menirav <nmenirav@gmail.com>
2025-12-30 16:28:13 +0200

    (Feat) is_on_grid function returns whether a point is on the grid or out of bounds

--- MDP/ex2.py
+++ MDP/ex2.py
@@ -10,7 +10,8 @@ class Controller:
         """Initialize controller for given game model."""
         self.original_game = game
 
-
+    # This function receives a point on the grid and returns a boolean value based on whether there is a robot in that
+    # coordinate
     def is_coordinate_contain_robot(self, coordinate, robots):
 
         for _, (r, c), _ in robots:
@@ -18,6 +19,8 @@ class Controller:
                 return True
         return False
 
+    # This function receives a point on the grid and returns a boolean value based on whether there is a wall in that
+    # coordinate
     def is_coordinate_contain_wall(self, coordinate):
 
         for wall in self.original_game.walls:
@@ -25,9 +28,24 @@ class Controller:
                 return True
         return False
 
+    # This function receives a point on the grid and returns a boolean value based on whether the point is a legal
+    # point on the gird
+    def is_on_grid(self, coordinate):
+        (r, c) = coordinate
+        return 0 <= r < self.original_game.rows and 0 <= c < self.original_game.cols
+
     def is_action_legal(self, state, action, robot_moving):
 
-        #if action == "UP":
+        (robots, plants, taps, total_water_needed) = state
+        (r, c) = robot_moving[1] # The coordinate of the robot
+
+        if action == "UP":
+            return (
+                    not self.is_coordinate_contain_robot((r - 1, c), robots)
+                    and not self.is_coordinate_contain_wall((r - 1, c))
+                    and self.is_on_grid((r - 1, c))
+            )
+
 
 
 
